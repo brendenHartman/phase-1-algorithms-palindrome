2545c630b3076cfbf42e2a6a9e6dfccf031e0421
Brenden Hartman <brendenhart22@gmail.com>
2024-01-02 11:17:38 -0600

    wow

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let result = false;
+  if(word.length % 2 === 0 ){
+    let backHalf = word.slice(word.length / 2);
+    let backHalfr = backHalf.split('').reverse().join('');
+    if(word.slice(0,word.length/2)===backHalfr){
+      result = true;
+    }
+  }
+  else{
+    let backHalf = word.slice(Math.ceil(word.length/2));
+    let backHalfre = backHalf.split('').reverse().join('');
+    if(word.slice(0,Math.floor(word.length/2)) === backHalfre){
+      result = true;
+    }
+  }
+  return result;
 }
 
 /* 
-  Add your pseudocode here
+initialize our result variable to be changed accordingly.
+  check if word has an even number or odd number of characters
+    if even cut the word in half
+      check the first half to the second half
+        change result accordingly
+    if odd take the first half and compare it to the second half excluding middle letter
+      check the two halves
+        change result accordingly
+  return result
+
 */
 
 /*
@@ -20,6 +44,10 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=> ", isPalindrome('bob'));
+
 }
 
 module.exports = isPalindrome;
